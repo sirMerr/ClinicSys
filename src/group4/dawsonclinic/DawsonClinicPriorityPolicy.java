a4665c2b2eaa5d97dcf0e64f1d9ef6d55a2ed5fa
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2015-11-21 17:03:47 -0500

    Change DawsonCLinicPriorityPolicy and test it

--- src/group4/dawsonclinic/DawsonClinicPriorityPolicy.java
+++ src/group4/dawsonclinic/DawsonClinicPriorityPolicy.java
@@ -3,6 +3,8 @@
  */
 package group4.dawsonclinic;
 
+import group4.clinic.business.Priority;
+
 import java.util.Optional;
 
 import dw317.clinic.business.interfaces.PriorityPolicy;
@@ -14,23 +16,18 @@ import dw317.clinic.data.interfaces.VisitDAO;
  * interface. The DawsonClinicPriorityPolicy class gets a VisitDAO object in its
  * constructor and it keeps a reference to this object as an instance variable.
  *
- * Priority 1 (REANIMATION) visit is always dequeued first. This means that you will
- * getNextVisit, remove, and then return the visit. If there are no Priority 1 visits, 
- * the remaining queues are dequeued in the following cycle, with the following constraint:
- * a patient with a lower priority is only dequeued if there is not a higher priority code patient 
- * who arrive before them (i.e., visit is compared based on registration time)
- * You will probably also need an instance variable to remember where you are in the
- * dequeue cycle:
- *	1. Priority 2 (VERYURGENT) visit dequeued
- *	2. Priority 3 (URGENT) visit dequeued
- *	3. Priority 2 visit dequeued
- *	4. Priority 4 (LESSURGENT) visit dequeued
- *	5. Priority 3 visit dequeued
- * 	6. Priority 2 visit dequeued
- *	7. Priority 5 (NOTURGENT) visit dequeued
- *	8. Priority 2 visit dequeued
- *	9. Priority 3 visit dequeued
- *	10. Priority 4 visit dequeued
+ * Priority 1 (REANIMATION) visit is always dequeued first. This means that you
+ * will getNextVisit, remove, and then return the visit. If there are no
+ * Priority 1 visits, the remaining queues are dequeued in the following cycle,
+ * with the following constraint: a patient with a lower priority is only
+ * dequeued if there is not a higher priority code patient who arrive before
+ * them (i.e., visit is compared based on registration time) You will probably
+ * also need an instance variable to remember where you are in the dequeue
+ * cycle: 1. Priority 2 (VERYURGENT) visit dequeued 2. Priority 3 (URGENT) visit
+ * dequeued 3. Priority 2 visit dequeued 4. Priority 4 (LESSURGENT) visit
+ * dequeued 5. Priority 3 visit dequeued 6. Priority 2 visit dequeued 7.
+ * Priority 5 (NOTURGENT) visit dequeued 8. Priority 2 visit dequeued 9.
+ * Priority 3 visit dequeued 10. Priority 4 visit dequeued
  *
  * @author Tiffany Le-Nguyen, Hugo Pham, Sevan Topalian, Andrew Azevedo
  * @since JDK.1.7
@@ -39,14 +36,62 @@ import dw317.clinic.data.interfaces.VisitDAO;
 public class DawsonClinicPriorityPolicy implements PriorityPolicy {
 
 	private static final long serialVersionUID = 42031768871L;
+	private VisitDAO visitDB;
+	private int position = 1;
 
 	public DawsonClinicPriorityPolicy(VisitDAO visitDB) {
-		
+		this.visitDB = visitDB;
 	}
+
 	@Override
 	public Optional<Visit> getNextVisit() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Optional<Visit> visit = null;
+		Priority priority = Priority.VERYURGENT;
+
+		//If visitDB is null, return empty Optional object
+		if (visitDB == null) {
+			return visit;
+		}
+		// If Prioriy 1 exists, return them and remove them.
+		while (visitDB.getNextVisit(Priority.REANIMATION) != null) {
+			visit = visitDB.getNextVisit(Priority.REANIMATION);
+			visitDB.remove(Priority.REANIMATION);
+			return visit;
+		}
+
+		//Algorithm for the priority dequeuing 
+		//If there is nothing and it reaches position 10, 
+		//there's an issue.
+		while (visitDB.getNextVisit(priority) == null && position <= 10) 
+		{
+			switch (position) {
+			
+			case 1: case 3: case 6:
+			case 8:
+				priority = Priority.VERYURGENT; //Priority 2
+				break;
+
+			case 2: case 5:
+			case 9:
+				priority = Priority.URGENT; //Priority 3
+				break;
+			case 4: case 10:
+				priority = Priority.LESSURGENT; //Priority 4
+				break;
+			case 7:
+				priority = Priority.NOTURGENT; //Priority 5
+				break;
+			}
+			position++;
+		}
+		if (position > 10) {
+			position = 0;
+		}
+		visit = visitDB.getNextVisit(priority);
+		visitDB.remove(priority);
+		
+		return visit;
 	}
 
 }
---
+++ test/group4/dawsonclinic/DawsonClinicPriorityPolicyTest.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package group4.dawsonclinic;
+
+import java.io.File;
+import java.io.IOException;
+
+import group4.clinic.data.SequentialTextFileList;
+import group4.clinic.data.VisitQueueDB;
+import group4.util.ListUtilities;
+
+/**
+ * @author Tiffany
+ *
+ */
+public class DawsonClinicPriorityPolicyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		setup();
+		VisitQueueDB visitQueue = new VisitQueueDB
+				(new SequentialTextFileList("testfiles/testPatients.txt",
+						"testfiles/testVisits.txt"));
+		DawsonClinicPriorityPolicy policyTest = new DawsonClinicPriorityPolicy(visitQueue);
+		
+		System.out.println(policyTest.getNextVisit());
+		
+		//teardown();
+	}
+	private static void setup()
+	{
+		String[] visits = new String[10];
+		visits [0] = "SMIM85122501*2015*9*1*13*30*******";
+		visits [1] = "RODM90571001*2015*9*1*14*45*******";
+		visits [2] = "LISH87100101*2015*9*1*13*20*" +
+				"2015*12*1*13*45*2*Severe rash";
+		visits [3] = "RAOV86112001*2015*9*1*13*50*" +
+				"2015*12*1*14*10*5*Bored";
+		File dir = new File("testfiles");
+		try{
+			if (!dir.exists()){
+				dir.mkdirs();
+			}
+			ListUtilities.saveListToTextFile(visits,
+					"testfiles/testVisits.txt");
+		}
+		catch(IOException io){
+			System.out.println
+			("Error creating file in setUp()");
+		}
+		
+		String[] patients = new String[10];
+		patients [0] = "LISH87100101*Shao*Li**" +
+				"DIN*02238645*292 tablets*Pain";
+		patients [1] = "RAOV86112001*Vishal*Rao*5143634564*" +
+				"NDC*43479-501-51*Pimple punisher*Acne";
+		//...
+		patients [2] = "RODM90571001*Maria*Rodriguez*5145555511****";
+		patients [3] = "SMIM85122501*Mike*Smith*5143634564*"+
+				"DIN*02239497*Absorbine Jr*Athlete’s foot";
+		patients [4] = "TOSH87100104*Shawn*To**" +
+				"DIN*02238645*SUPER TABLETS*Pain";
+
+		try {
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			ListUtilities.saveListToTextFile(patients,
+					"testfiles/testPatients.txt");
+		} catch (IOException io) {
+			System.out.println("Error creating file in setUp()");
+		}
+	}
+	/**
+	 * Removes the test cases.
+	 */
+	private static void teardown() {
+		File theFile = new File("testfiles/testVisits.txt");
+		if (theFile.exists()) {
+			theFile.delete();
+		}
+	}
+	
+
+}
